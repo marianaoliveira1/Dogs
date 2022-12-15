ed6dca268c40161f599a3dfcb4a65bd95a01b40f
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2022-12-15 17:51:01 -0300

    header feito e estilizado

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from '../styles/Header.module.css'
+import {ReactComponent as Dogs} from '../assets/dogs.svg'
 
 export default function Header() {
   return (
-    <div className={styles.header}>
-        <nav className='container'>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+        <nav className={`${styles.nav} container`}>
+            <Link className={styles.logo} to="/" aria-label='Dogs - Home'>
+                <Dogs />
+            </Link>
+            <Link className={styles.login} to="/login">Login / Criar</Link>
         </nav>
-    </div>
+    </header>
   )
 }
