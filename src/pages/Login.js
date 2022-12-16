23440562f9c7a4bde96aa524ae54ea6636c9d4c4
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2022-12-16 11:34:18 -0300

    rotas de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+
 import React from 'react'
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginPasswordLost from './LoginPasswordLost'
+import LoginPasswordReset from './LoginPasswordReset'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<LoginForm />} />
+        <Route path='/criar' element={<LoginCreate />} />
+        <Route path='/perdeu' element={<LoginPasswordLost />} />
+        <Route path='/resetar' element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
   )
 }
---
+++ src/pages/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function LoginForm() {
+  return (
+    <div>
+        LoginForm
+        <Link to="/login/criar">Cadastrar</Link>
+    </div>
+  )
+}
