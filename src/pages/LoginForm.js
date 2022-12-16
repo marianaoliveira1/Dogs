d1548ef7630f6d680b9e9b5b79edc1e1404d43b1
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2022-12-16 11:48:22 -0300

    form login e autenticacao

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -2,10 +2,44 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function LoginForm() {
+    const [username, setUserName] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, password}),
+        }).then((response) => {
+            console.log(response);
+            return response.json();
+        }).then((json) => {
+            console.log(json);
+        });
+    }
+
   return (
     <div>
-        LoginForm
-        <Link to="/login/criar">Cadastrar</Link>
+        <section>
+            <h1>Login</h1>
+            <form action='' onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={({target}) => setUserName(target.value)}
+                />
+                <input
+                    type="text"
+                    value={password}
+                    onChange={({target}) => setPassword(target.value)}
+                />
+                <button>Entrar</button>
+            </form>
+            <Link to="/login/criar">Cadastrar</Link>
+        </section>
     </div>
   )
 }
